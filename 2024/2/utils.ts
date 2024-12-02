69a6ee8ba1f44d4971f1d2c8cd07d9b34eb79983
llNicoleta <idlnicoleta@gmail.com>
2024-12-02 21:08:59 +0200

    refactor: extract common method to utils.

--- 2024/2/utils.ts
+++ 2024/2/utils.ts
@@ -15,3 +15,10 @@ export function isDescending(arr: number[]): boolean {
         return num < arr[index - 1] && num > arr[index + 1];
     })
 }
+
+export function isDistanceAtMostThree(nums: number[]) {
+    return nums.every((num, index) => {
+        if (index === 0) return true;
+        return [1, 2, 3].includes(Math.abs(num - nums[index - 1]));
+    })
+}
